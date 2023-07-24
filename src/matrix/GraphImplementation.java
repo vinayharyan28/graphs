60197bf363312d7a97e66629b8cb0bca8d708ab3
vinayharyan28 <vinayharyan28@gmail.com>
2023-07-24 22:23:29 +0530

    Add BFS and DFS

---
+++ src/matrix/GraphImplementation.java
@@ -0,0 +1,49 @@
+package matrix;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GraphImplementation {
+    static void addMatrixEdge(int[][] graphMatrix, int source, int destination){
+        graphMatrix[source][destination] = 1;
+        graphMatrix[destination][source] = 1;
+    }
+
+    static void addAdjacencyList(ArrayList<ArrayList<Integer>> adj, int source, int destination){
+        adj.get(source).add(destination);
+        adj.get(destination).add(source);
+    }
+
+    static void printMatrix(int[][] graphMatrix){
+        for (int[] row: graphMatrix){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void main(String[] args) {
+        int vertex = 5;
+
+        // matrix
+        int[][] graphMatrix = new int[vertex+1][vertex+1];
+        addMatrixEdge(graphMatrix, 3, 5);
+        addMatrixEdge(graphMatrix, 5, 4);
+        addMatrixEdge(graphMatrix, 5, 1);
+        addMatrixEdge(graphMatrix, 4, 2);
+        addMatrixEdge(graphMatrix, 4, 1);
+        addMatrixEdge(graphMatrix, 1, 2);
+        printMatrix(graphMatrix);
+
+        // adjacency list
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i=0; i<=vertex; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        addAdjacencyList(adj, 3, 5);
+        addAdjacencyList(adj, 5, 4);
+        addAdjacencyList(adj, 5, 1);
+        addAdjacencyList(adj, 4, 2);
+        addAdjacencyList(adj, 4, 1);
+        addAdjacencyList(adj, 1, 2);
+        System.out.println(adj);
+    }
+}
---
+++ src/matrix/GraphTraversal.java
@@ -0,0 +1,79 @@
+package matrix;
+import java.util.*;
+
+
+public class GraphTraversal {
+    static void addEdge(ArrayList<ArrayList<Integer>> graph, int source, int destination){
+        graph.get(source).add(destination);
+        graph.get(destination).add(source);
+    }
+
+    static void createGraph(ArrayList<ArrayList<Integer>> graph, int vertex){
+        for (int i=0; i<=vertex; i++){
+            graph.add(new ArrayList<>());
+        }
+    }
+
+    static void printGraph(ArrayList<ArrayList<Integer>> graph){
+        for (ArrayList<Integer> rows: graph){
+            System.out.println(rows);
+        }
+    }
+
+    static void bfs(ArrayList<ArrayList<Integer>> graph, int source){
+        boolean[] visited = new boolean[7];
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+
+        while (queue.size()!=0){
+            int element = queue.poll();
+            System.out.print(element + " ");
+            for (int n: graph.get(element)){
+                if (!visited[n]){
+                    visited[n] = true;
+                    queue.offer(n);
+                }
+            }
+        }
+    }
+
+    static void dfs(ArrayList<ArrayList<Integer>> graph, int vertex, boolean[] visited){
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+        for (int neighbor: graph.get(vertex)){
+            if (!visited[neighbor]){
+                dfs(graph, neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        createGraph(graph, 6);
+
+        /*
+        addEdge(graph, 1, 3);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 1, 5);
+        addEdge(graph, 3, 4);
+        addEdge(graph, 4, 6);
+        addEdge(graph, 5, 6);
+        addEdge(graph, 4, 5);
+        */
+
+        addEdge(graph, 0, 1);
+        addEdge(graph, 0, 4);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 1, 3);
+        addEdge(graph, 2, 5);
+        addEdge(graph, 3, 6);
+        addEdge(graph, 5, 6);
+        addEdge(graph, 4, 3);
+        printGraph(graph);
+        bfs(graph, 1);
+        System.out.println();
+        System.out.println("DFS");
+        boolean[] visited = new boolean[7];
+        dfs(graph, 0, visited);
+    }
+}
