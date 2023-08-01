99086d8c54409cd6755baea83fdb6cb0617caf42
vinayharyan28 <vinayharyan28@gmail.com>
2023-08-01 22:23:10 +0530

    Bellman ford undirected graph

--- src/graphconcept/BellmanFordAlgorithmUndirected.java
+++ src/graphconcept/BellmanFordAlgorithmUndirected.java
@@ -44,10 +44,10 @@ public class BellmanFordAlgorithmUndirected {
         ArrayList<Edge> graph = new ArrayList<>();
         addEdge(graph, 0, 1, 5);
         addEdge(graph, 0, 2, 3);
-        addEdge(graph, 1, 2, 2);
+        addEdge(graph, 1, 2, -2);
         addEdge(graph, 2, 3, 7);
         addEdge(graph, 2, 4, 4);
-        addEdge(graph, 3, 4, -10);
+        addEdge(graph, 3, 4, 10);
         int vertices=5, source=0;
         int[] distance = bellmanFord(source, vertices, graph);
         for (int i=0; i<distance.length; i++){
